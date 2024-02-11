c9fc5c51520568b85b8a27c947e2b32f2677267e
Shimanto Bhowmik <bmikshimanto@gmail.com>
2024-02-11 17:05:50 -0500

    Added && , ; functionality

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,12 @@
-use std::process::Command;
+use std::process::{Command, Stdio, Child};
 use std::io::{stdin, stdout, Write};
 use std::path::Path;
-use std::process::Stdio;
-use std::process::Child;
 use std::env;
 
-fn main(){
+fn main() {
     let mut history: Vec<String> = Vec::new();
     loop {
-        print!("R-> ");
+        print!("r@shell > ");
         stdout().flush().unwrap();
 
         let mut input = String::new();
@@ -19,6 +17,7 @@ fn main(){
             continue;
         }
 
+        //support for multiline commands
         while input.ends_with('\\') {
             input.pop();
             print!("> ");
@@ -33,66 +32,83 @@ fn main(){
             history.push(input.trim().to_string());
         }
 
-        let mut commands = input.trim().split(" | ").peekable();
-        let mut previous_command = None;
+        //support for && and ;
+        let command_blocks = input.trim().split_inclusive(|c| c == '&' || c == ';');
+        for block in command_blocks {
+            let block = block.trim_matches(|c| c == '&' || c == ';').trim();
+            if block.is_empty() {
+                continue;
+            }
 
-        while let Some(command) = commands.next()  {
+            let mut previous_command: Option<Child> = None;
+            let mut commands = block.split(" | ").peekable();
+            let mut block_success = true;
 
-            let mut parts = command.trim().split_whitespace();
-            let command = parts.next().unwrap();
-            let args = parts;
+            while let Some(command) = commands.next() {
+                let mut parts = command.trim().split_whitespace();
+                let command = parts.next().unwrap();
+                let args = parts;
 
-            match command {
-                "cd" => {
-                    let new_dir = args.peekable().peek()
-                        .map_or("/", |x| *x);
-                    let root = Path::new(new_dir);
-                    if let Err(e) = env::set_current_dir(&root) {
-                        eprintln!("{}", e);
-                    }
-
-                    previous_command = None;
-                },
-                "exit" => return,
-                "history" => {
-                    for (index, command) in history.iter().enumerate() {
-                        println!("{}. {}", index + 1, command);
-                    }
-                    previous_command = None;
-                },
-                command => {
-                    let stdin = previous_command
-                        .map_or(
-                            Stdio::inherit(),
-                            |output: Child| Stdio::from(output.stdout.unwrap())
-                        );
-
-                    let stdout = if commands.peek().is_some() {
-                        Stdio::piped()
-                    } else {
-                        Stdio::inherit()
-                    };
-
-                    let output = Command::new(command)
-                        .args(args)
-                        .stdin(stdin)
-                        .stdout(stdout)
-                        .spawn();
-
-                    match output {
-                        Ok(output) => { previous_command = Some(output); },
-                        Err(e) => {
-                            previous_command = None;
+                match command {
+                    "cd" => {
+                        let new_dir = args.peekable().peek().map_or("/", |x| *x);
+                        let root = Path::new(new_dir);
+                        if let Err(e) = env::set_current_dir(&root) {
                             eprintln!("{}", e);
-                        },
-                    };
+                            block_success = false;
+                        }
+                        previous_command = None;
+                    },
+                    "exit" => return,
+                    "history" => {
+                        for (index, command) in history.iter().enumerate() {
+                            println!("{}. {}", index + 1, command);
+                        }
+                        previous_command = None;
+                    },
+                    _ => {
+                        let stdin = previous_command
+                            .take()
+                            .map_or(Stdio::inherit(), |output: Child| Stdio::from(output.stdout.unwrap()));
+
+                        let stdout = if commands.peek().is_some() {
+                            Stdio::piped()
+                        } else {
+                            Stdio::inherit()
+                        };
+
+                        let child = Command::new(command)
+                            .args(args)
+                            .stdin(stdin)
+                            .stdout(stdout)
+                            .spawn();
+
+                        match child {
+                            Ok(child) => { previous_command = Some(child); },
+                            Err(e) => {
+                                eprintln!("No such command.");
+                                eprintln!("Error: {}", e);
+                                block_success = false;
+                                break;
+                            },
+                        }
+                    }
                 }
             }
-        }
 
-        if let Some(mut final_command) = previous_command {
-            let _ = final_command.wait();
-        }
+            if let Some(mut final_command) = previous_command {
+                if let Ok(exit_status) = final_command.wait() {
+                    if !exit_status.success() {
+                        block_success = false;
+                    }
+                } else {
+                    block_success = false;
+                }
+            }
 
+            if block.ends_with("&&") && !block_success {
+                break;
+            }
+        }
     }
 }
