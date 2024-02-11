6e1e7e96a8daa7df01cf7dcab3f50b1438c785af
Shimanto Bhowmik <bmikshimanto@gmail.com>
2024-02-11 15:45:43 -0500

    Error handling included

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,40 @@
 use std::process::Command;
-use std::io::{stdin};
+use std::io::{stdin, stdout, Write};
+use std::path::Path;
 
 fn main() {
-    let mut input_command = String::new();
-    stdin().read_line(&mut input_command).unwrap();
-    let input_command = input_command.trim();
-    Command::new(input_command)
-        .spawn()
-        .unwrap();
+    loop {
+        print!("@ ");
+        stdout().flush().unwrap();
 
+        let mut input_command = String::new();
+        stdin().read_line(&mut input_command).unwrap();
+        let mut input = input_command.trim().split_whitespace();
+        let command = input.next().unwrap();
+        let args = input;
+
+        match command {
+            "cd" => {
+                let new_dir = args.peekable().peek().map_or("/", |x| *x);
+                let root = Path::new(new_dir);
+                if let Err(e) = std::env::set_current_dir(&root) {
+                    eprintln!("{}", e);
+                }
+            },
+            "exit" => return,
+            command =>{
+                let child = Command::new(command)
+                    .args(args)
+                    .spawn();
+                
+                //error handle
+                match child {
+                    Ok(mut child) => { 
+                        let _ = child.wait(); 
+                    },
+                    Err(e) => eprintln!("{}", e),
+                };
+            }
+        }
+    }
 }
